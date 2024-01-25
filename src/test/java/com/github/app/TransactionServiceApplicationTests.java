9f1a8fb3a112fd2ec94cae6cc3831101a0e5f46b
muammarzaki <muammarzaki2004@gmail.com>
2024-01-25 19:35:42 +0700

    fixing main test

--- src/test/java/com/github/app/TransactionServiceApplicationTests.java
+++ src/test/java/com/github/app/TransactionServiceApplicationTests.java
@@ -1,9 +1,18 @@
 package com.github.app;
 
+import com.github.security.AuthenticationController;
+import com.github.security.Encoding;
+import com.github.security.UserDetailServiceImp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes = {
+	AuthenticationController.class,
+	UserDetailServiceImp.class,
+	Encoding.class
+})
 class TransactionServiceApplicationTests {
 
 	@Test
