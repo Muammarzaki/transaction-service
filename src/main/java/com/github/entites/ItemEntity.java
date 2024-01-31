4db20a434978c29dfc0bf95cdeb90e2895c61d4e
muammarzaki <muammarzaki2004@gmail.com>
2024-01-31 21:00:23 +0700

    add entity and repository for Item

--- src/main/java/com/github/entites/ItemEntity.java
+++ src/main/java/com/github/entites/ItemEntity.java
@@ -1,13 +1,22 @@
 package com.github.entites;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 @Table(name = "item")
-public record ItemEntity(
-	@Id@GeneratedValue(strategy = GenerationType.IDENTITY) Long id,
-	String ItemId,
-	int count,
-	int price
-) {
+public class ItemEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String ItemId;
+	private int count;
+	private int price;
 }
