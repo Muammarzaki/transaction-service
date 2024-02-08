4247d1474dfff9c7207934f96b2f8aead32c27fb
muammarzaki <muammarzaki2004@gmail.com>
2024-02-08 20:09:26 +0700

    add transaction response and some test of it

---
+++ src/main/java/com/github/domain/MidtransDomain.java
@@ -0,0 +1,165 @@
+package com.github.domain;
+
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+import java.util.Collections;
+import java.util.Currency;
+import java.util.List;
+
+public abstract class MidtransDomain {
+
+
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "payment_type")
+	@JsonSubTypes({
+		@JsonSubTypes.Type(value = CStoreResponse.class, name = "cstore"),
+		@JsonSubTypes.Type(value = EWalletResponse.class, name = "qris"),
+		@JsonSubTypes.Type(value = EWalletResponse.class, name = "gopay"),
+		@JsonSubTypes.Type(value = BankTransferResponse.class, name = "bank_transfer")
+	})
+	public static class TransactionResponse {
+		@NotNull(message = "must provide the value")
+		@PositiveOrZero(message = "gross amount should have 0 or greater")
+		int grossAmount;
+		@NotBlank(message = "cannot blank or null")
+		String orderId;
+		@NotBlank(message = "cannot blank or null")
+		Currency Currency;
+		@NotBlank(message = "cannot blank or null")
+		@JsonProperty("payment_type")
+		String paymentType;
+		@NotBlank(message = "cannot blank or null")
+		String transactionId;
+		@NotBlank(message = "cannot blank or null")
+		String transactionStatus;
+		@NotBlank(message = "cannot blank or null")
+		String transactionTime;
+		@NotBlank(message = "cannot blank or null")
+		String fraudStatus;
+
+	}
+
+	@JsonTypeName("cstore")
+	@EqualsAndHashCode(callSuper = true)
+	@Data
+	@NoArgsConstructor
+	@Builder
+	@AllArgsConstructor
+	public static class CStoreResponse extends TransactionResponse {
+		private String paymentCode;
+		private String store;
+	}
+
+	@JsonTypeName("qris")
+	@EqualsAndHashCode(callSuper = true)
+	@Data
+	@NoArgsConstructor
+	@Builder
+	@AllArgsConstructor
+	public static class EWalletResponse extends TransactionResponse {
+		private List<Action> actions;
+
+		@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+		public record Action(
+			String name,
+			String method,
+			String url
+		) {
+		}
+	}
+
+	@JsonTypeName("bank_transfer")
+	@EqualsAndHashCode(callSuper = true)
+	@Data
+	@NoArgsConstructor
+	@Builder
+	@AllArgsConstructor
+	public static class BankTransferResponse extends TransactionResponse {
+		private List<VaNumber> vaNumbers;
+
+		@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+		public record VaNumber(
+			String bank,
+			String vaNumber
+		) {
+		}
+	}
+
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public record TransactionDetails(
+		@NotNull(message = "must provide the value")
+		@PositiveOrZero(message = "gross amount should have 0 or greater")
+		int grossAmount,
+		@NotBlank(message = "cannot blank or null")
+		String orderId
+	) {
+	}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public record CustomerDetails(
+		@NotBlank(message = "cannot blank or null")
+		String name,
+		@NotBlank(message = "cannot blank or null")
+		@Email(message = "email not valid")
+		String email
+	) {
+	}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public record Items(
+		@NotBlank(message = "cannot blank or null")
+		String itemId,
+		@NotBlank(message = "cannot blank or null")
+		String itemName,
+		@Min(value = 1, message = "Minimum values is one")
+		@Positive(message = "must positive value")
+		int count,
+		@Min(value = 1, message = "Minimum values is one")
+		@Positive(message = "must positive value")
+		int price) {
+	}
+
+	@Getter
+	public enum PaymentMethod {
+		CREDIT_CARD("credit_card", Collections.emptyList()),
+		CSTORE("cstore", List.of("alfamart", "indomaret")),
+		QRIS("qris", List.of("gopay", "qris")),
+		BANK_TRANSFER("bank_transfer", List.of("bni"));
+
+		private final String type;
+		private final List<String> subType;
+
+		PaymentMethod(String creditCard, List<String> subType) {
+			type = creditCard;
+			this.subType = subType;
+		}
+
+		public static PaymentMethod of(String type) {
+			for (PaymentMethod method : PaymentMethod.values()) {
+				if (method.type.equalsIgnoreCase(type)) {
+					return method;
+				}
+			}
+			throw new IllegalArgumentException(String.format("Unknown PaymentMethod type: %s", type));
+		}
+	}
+
+	@JsonIgnoreProperties(ignoreUnknown = true)
+	@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+	public record Cstore(
+		@NotBlank(message = "cannot blank or null")
+		@Pattern(regexp = "indomaret|alfamart")
+		String store,
+		String message
+	) {
+	}
+}
---
+++ src/test/java/com/github/domain/MidtransDomainTest.java
@@ -0,0 +1,130 @@
+package com.github.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class MidtransDomainTest {
+	String cstoreResponse;
+	String bankResponse;
+	String eWalletResponse;
+
+	ObjectMapper mapper;
+
+	@BeforeEach
+	void setUp() {
+		mapper = new ObjectMapper();
+		cstoreResponse = """
+			{
+			  "status_code": "201",
+			  "status_message": "Success, cstore transaction is successful",
+			  "transaction_id": "d615df87-c96f-4f5c-9d35-2d740d54c1a9",
+			  "order_id": "order-101o-1578557780",
+			  "merchant_id": "G812785002",
+			  "gross_amount": "162500.00",
+			  "currency": "IDR",
+			  "payment_type": "cstore",
+			  "transaction_time": "2020-01-09 15:16:19",
+			  "transaction_status": "pending",
+			  "fraud_status": "accept",
+			  "payment_code": "8127740588870520",
+			  "store": "alfamart"
+			}""";
+		bankResponse = """
+			{
+			  "status_code": "201",
+			  "status_message": "Success, Bank Transfer transaction is created",
+			  "transaction_id": "be03df7d-2f97-4c8c-a53c-8959f1b67295",
+			  "order_id": "1571823229",
+			  "merchant_id": "G812785002",
+			  "gross_amount": "44000.00",
+			  "currency": "IDR",
+			  "payment_type": "bank_transfer",
+			  "transaction_time": "2019-10-23 16:33:49",
+			  "transaction_status": "pending",
+			  "va_numbers": [
+			    {
+			      "bank": "bca",
+			      "va_number": "812785002530231"
+			    }
+			  ],
+			  "fraud_status": "accept"
+			}""";
+		eWalletResponse = """
+			{
+			  "status_code": "201",
+			  "status_message": "GO-PAY transaction is created",
+			  "transaction_id": "231c79c5-e39e-4993-86da-cadcaee56c1d",
+			  "order_id": "order-101h-1570513296",
+			  "gross_amount": "44000.00",
+			  "currency": "IDR",
+			  "payment_type": "qris",
+			  "transaction_time": "2019-10-08 12:41:36",
+			  "transaction_status": "pending",
+			  "fraud_status": "accept",
+			  "actions": [
+			    {
+			      "name": "generate-qr-code",
+			      "method": "GET",
+			      "url": "https://api.sandbox.veritrans.co.id/v2/gopay/231c79c5-e39e-4993-86da-cadcaee56c1d/qr-code"
+			    },
+			    {
+			      "name": "deeplink-redirect",
+			      "method": "GET",
+			      "url": "https://simulator.sandbox.midtrans.com/gopay/ui/checkout?referenceid=Y0xwjoQ9uy&callback_url=someapps%3A%2F%2Fcallback%3Forder_id%3Dorder-101h-1570513296"
+			    },
+			    {
+			      "name": "get-status",
+			      "method": "GET",
+			      "url": "https://api.sandbox.veritrans.co.id/v2/231c79c5-e39e-4993-86da-cadcaee56c1d/status"
+			    },
+			    {
+			      "name": "cancel",
+			      "method": "POST",
+			      "url": "https://api.sandbox.veritrans.co.id/v2/231c79c5-e39e-4993-86da-cadcaee56c1d/cancel"
+			    }
+			  ]
+			}""";
+	}
+
+	@Test
+	void shouldMapperToCStoreDomain() {
+		assertDoesNotThrow(() -> {
+			MidtransDomain.TransactionResponse transactionResponse = mapper.readValue(cstoreResponse, MidtransDomain.TransactionResponse.class);
+			assertThat(transactionResponse).isInstanceOf(MidtransDomain.CStoreResponse.class)
+				.extracting("paymentCode").isNotNull();
+		});
+	}
+
+	@Test
+	void shouldMapperToBankTransferDomain() {
+		assertDoesNotThrow(() -> {
+			MidtransDomain.TransactionResponse transactionResponse = mapper.readValue(bankResponse, MidtransDomain.TransactionResponse.class);
+			assertThat(transactionResponse).isInstanceOf(MidtransDomain.BankTransferResponse.class)
+				.asInstanceOf(InstanceOfAssertFactories.type(MidtransDomain.BankTransferResponse.class))
+				.extracting(MidtransDomain.BankTransferResponse::getVaNumbers).isNotNull()
+				.asInstanceOf(InstanceOfAssertFactories.list(MidtransDomain.EWalletResponse.Action.class))
+				.hasSize(1);
+		});
+	}
+
+	@Test
+	void shouldMapperToEWalletDomain() {
+		assertDoesNotThrow(() -> {
+			MidtransDomain.TransactionResponse transactionResponse = mapper.readValue(eWalletResponse, MidtransDomain.TransactionResponse.class);
+			assertThat(transactionResponse).isInstanceOf(MidtransDomain.EWalletResponse.class)
+				.asInstanceOf(InstanceOfAssertFactories.type(MidtransDomain.EWalletResponse.class))
+				.extracting(MidtransDomain.EWalletResponse::getActions)
+				.asInstanceOf(InstanceOfAssertFactories.list(MidtransDomain.EWalletResponse.Action.class))
+				.hasSize(4);
+		});
+	}
+
+
+}
