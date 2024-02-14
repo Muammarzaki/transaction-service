bd1d66c7b698a28b5dbde807047ff2b6f51f8436
muammarzaki <muammarzaki2004@gmail.com>
2024-02-14 21:08:36 +0700

    add configuration test for objectMapper

--- src/test/java/com/github/app/JacksonConfigurationTest.java
+++ src/test/java/com/github/app/JacksonConfigurationTest.java
@@ -3,9 +3,20 @@ package com.github.app;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = JacksonConfiguration.class)
+@EnableAutoConfiguration
 public class JacksonConfigurationTest {
+	@Autowired
+	ObjectMapper mapper;
 
 	public static ObjectMapper getConfig() {
 		return new ObjectMapper()
@@ -14,4 +25,9 @@ public class JacksonConfigurationTest {
 			.registerModule(new JavaTimeModule());
 	}
 
+	@Test
+	void mapperConfigurationValidators() {
+		PropertyNamingStrategy propertyNamingStrategy = mapper.getPropertyNamingStrategy();
+		assertThat(propertyNamingStrategy).isInstanceOf(PropertyNamingStrategies.SnakeCaseStrategy.class);
+	}
 }
